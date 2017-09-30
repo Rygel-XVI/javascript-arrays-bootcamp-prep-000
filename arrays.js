08112b099e5db95865a6a6c98ea9a935f7d48ae1
Annette Michaels <micha232@umn.edu>
2017-09-30 19:24:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+function addElementToBeginningOfArray() {
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
